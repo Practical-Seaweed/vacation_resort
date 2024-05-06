75f0f5c7a5afa594d70db6701b07acc66ecc6749
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-06 15:59:57 -0500

    IT WORKSS HAHAHAAA now to do the discounts and the tax

--- scripts/overnightStay.js
+++ scripts/overnightStay.js
@@ -1,19 +1,21 @@
-"use strict"
+"use strict";
 
-window.onload = function(){
+window.onload = function () {
 
     let estimateForm = document.querySelector("#estimateStayForm");
 
     estimateForm.addEventListener("submit", estimateStay);
 
+    console.log("hello?? testing??")
+
 }
 
-function estimateStay(event){
+function estimateStay(event) {
     event.preventDefault();
 
     let estimateForm = event.target;
 
-    let roomTotal = Number(estimateForm.numberOfNights.value) * getRoomRate(estimateForm.checkInDate.value, estimateForm.roomType.value);
+    let roomTotal = Number(estimateForm.numberofNights.value) * getRoomRate(estimateForm.checkInDate.value, estimateForm.roomType.value);
 
     console.log(roomTotal);
 
@@ -23,19 +25,53 @@ function estimateStay(event){
     //lets see the date they submitted
     console.log(estimateForm.checkInDate.value)
 
+
+
+    let message = `
+    <div>The original room cost: $${roomTotal.toFixed(2)} </div>
+    <div>The discount, if any: </div>
+    <div>The discounted room cost: </div>
+    <div>The tax</div>
+    <div>The total cost of the stay: </div>
+    `
+
+    document.querySelector("#results").innerHTML = message;
 }
 
 
 //this is the function you put the logic to determine the actual rate
-function getRoomRate(checkInDate, roomType){
+function getRoomRate(checkInDate, roomType) {
 
-    //logic goes here to determine the room rate based on the check-in date and room type
-    //for now just return 150
+    let checkInMonth = new Date(checkInDate).getMonth();
 
-    if(roomType === "suite"){
-        return 250;
+    // this works for June - August per night. gotta figure out how to do for rest of year
+
+    if (checkInMonth >= 5 && checkInMonth <= 7) {
+        if (roomType === "queen") {
+            return 250;
+        }
+        else if (roomType === "king") {
+            return 250;
+        }
+        else if (roomType === "suite") {
+            return 350;
+        }
+        else {
+            return 150;
+        }
+    } else {
+        if (roomType === "queen") {
+            return 150;
+        }
+        else if (roomType === "king") {
+            return 150;
+        }
+        else {
+            roomType === "king"
+        }
+        return 210;
     }
 
-    return 150;
+
 
 }
